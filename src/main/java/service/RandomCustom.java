1d7374b5b6dd14b20db844538404023427246278
Vitalii_Tymashkov <vitaliy.tima@gmail.com>
2020-04-04 19:58:35 +0200

    Dev for method 3

--- src/main/java/service/RandomCustom.java
+++ src/main/java/service/RandomCustom.java
@@ -2,10 +2,68 @@ package service;
 
 public class RandomCustom {
 
+    //Normal delta for java random = 0.05%
+
     public int rand7_1(int first, int second){
-        return first + second % 3;
+        return first + second % 3;                  //DELTA = 16% - most used = from 2 to 6
     }
+
     public int rand7_2(int first, int second){
-        return ((first + second - 2) % 7) + 1;
+        return ((first + second - 2) % 7) + 1;      //DELTA = 8% - most used = 5
+//        return ((first + second) % 7) + 1;        //DELTA = 8% - most used = 7
     }
+
+    public int rand7_3(int first){
+//        return 1;                                 // missing 2-7 (delta 0%)
+//        return first;                             // missing 6, 7 (delta 0.05%)
+//        return (int) (Math.round(first * 1.4));   // missing 2, 5 (delta 0.15%)
+//        return (int) (first*1.4) % 8;             // missing 3, 6 (delta 0.15%)
+//        return (((first - 1) * 2) % 7) + 1;       // missing 4, 6 (delta 0.05%)
+//        return (first*10) % 7 + 1;                // missing 1, 5 (delta 0.02%)
+//        return ((first - 1) * 2) % 7 + 1;         // missing 4, 6 (delta 0.10%)
+//        return ((first - 1) * 222) % 7 + 1;       // missing 3, 5 (delta 0.07%)
+//        return first + (first - 1) % 3;           //missing 2, 7 (delta 0.12%)
+//        return first + (first) % 2;               //missing 1, 3, 5, 7 (delta 20%)
+//        return first + (first) % 3;               //missing 1, 6 (delta 0.08%)
+//        return first + (first-1) % 3;             //missing 2, 7 (delta 0.13%)
+//        return first + (first-1) % 2;             //missing 2, 4, 6, 7 (delta 20%)
+//        return ((first-1)%3 + (first-1)%4) + 1;   //missing 6, 7 (delta 0.08%)
+//        return ((first-1)%3 ) + 1;   //missing 4- 7 (delta 20%)
+//        return (((first-1)%2 + (first-1)%3 + (first-1)%4 + (first-1)%5))%7 + 1;             //missing 6, 7 (delta 0.08%)
+
+        /*
+        1 2 3 4 5
+        0001-0010-0011-0100-0101
+        000-100-100-011-010-001-01
+        0000-0100-0100-0011-0010-0001-0001
+        0 4 4 3 2 1 1
+
+            1 2 3 4 5 6 7
+
+        1.4
+        2.8
+        4.2
+        5.8
+        7.0
+
+        */
+        double worker = (first) * Math.PI;
+
+        int result = 0;
+
+
+
+        String workerString = String.valueOf(worker).replace(".","").substring(0,15);
+        int length = workerString.length();
+
+        for (int i = 0; i < length; i++){
+            char c = workerString.charAt(i);
+            result = result + c;
+        }
+
+
+
+        return result % 7 + 1;
+    }
+
 }
--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -8,28 +8,44 @@ public class Service {
     public void start() {
 
         CounterCustom counterCustom1 = new CounterCustom();
-        CounterCustom counterCustom2 = new CounterCustom();
-        CounterCustom counterCustomForFirst = new CounterCustom();
-        CounterCustom counterCustomForSecond = new CounterCustom();
         Map<Integer, Integer> count1 = new HashMap<>();
+        List<Integer[]> result1 = new ArrayList<>(TEST_NUMBER);
+
+        CounterCustom counterCustom2 = new CounterCustom();
         Map<Integer, Integer> count2 = new HashMap<>();
+        List<Integer[]> result2 = new ArrayList<>(TEST_NUMBER);
+
+        CounterCustom counterCustom3 = new CounterCustom();
+        Map<Integer, Integer> count3 = new HashMap<>();
+        List<Integer[]> result3 = new ArrayList<>(TEST_NUMBER);
+
+        CounterCustom counterCustomForFirst = new CounterCustom();
         Map<Integer, Integer> countForFirst = new HashMap<>();
+
+        CounterCustom counterCustomForSecond = new CounterCustom();
         Map<Integer, Integer> countForSecond = new HashMap<>();
 
-        List<Integer[]> result1 = new ArrayList<>(TEST_NUMBER);
-        List<Integer[]> result2 = new ArrayList<>(TEST_NUMBER);
+        CounterCustom counterCustomForThird = new CounterCustom();
+        Map<Integer, Integer> countForThird = new HashMap<>();
+
 
 
         RandomCustom randomCustom1 = new RandomCustom();
         int resultRandomCustom1;
         int resultRandomCustom2;
+        int resultRandomCustom3;
 
 
         for (int i = 0; i < TEST_NUMBER; i++) {
+
             int first = new Random().nextInt(5) + 1;
             counterCustomForFirst.count(countForFirst, first);
+
             int second = new Random().nextInt(5) + 1;
             counterCustomForSecond.count(countForSecond, second);
+
+            int third = new Random().nextInt(5) + 1;
+            counterCustomForThird.count(countForThird, third);
 
             resultRandomCustom1 = randomCustom1.rand7_1(first, second);
             Integer[] resultArray1 = new Integer[] {first, second, resultRandomCustom1};
@@ -40,40 +56,51 @@ public class Service {
             Integer[] resultArray2 = new Integer[] {first, second, resultRandomCustom2};
             result2.add(resultArray2);
             counterCustom2.count(count2, resultRandomCustom2);
+
+            resultRandomCustom3 = randomCustom1.rand7_3(first);
+            Integer[] resultArray3 = new Integer[] {first, second, resultRandomCustom3};
+            result3.add(resultArray3);
+            counterCustom3.count(count3, resultRandomCustom3);
         }
 
 //        showResults(result1, result2);
 
-        System.out.println("for rand7");
-        showStatistics(count1, count2);
-        showDelta(counterCustom1, counterCustom2, count1, count2);
+        System.out.println("            for rand7");
+        showStatistics(count1, count2, count3);
+        showDelta(counterCustom1, counterCustom2, counterCustom3, count1, count2, count3);
 
-        System.out.println("for rand5");
-        showStatistics(countForFirst, countForSecond);
-        showDelta(counterCustomForFirst, counterCustomForSecond, countForFirst, countForSecond);
+
+//        System.out.println("            for rand5");
+//        showStatistics(countForFirst, countForSecond, countForThird);
+//        showDelta(counterCustomForFirst, counterCustomForSecond, counterCustomForThird, countForFirst, countForSecond, countForThird);
 
 
     }
 
-    private void showDelta(CounterCustom counterCustom1, CounterCustom counterCustom2, Map<Integer, Integer> count1, Map<Integer, Integer> count2) {
-        System.out.println("********************************************");
-        System.out.println("               Delta");
-        System.out.println("|          ||| Of method 1 | Of method 2 | ");
-        System.out.println("|----------|||-------------|-------------| ");
-        System.out.println(String.format("|  Delta   ||| %11d | %11d |", counterCustom1.countDelta(count1), counterCustom2.countDelta(count2)));
-        System.out.println("|----------------------------------------| ");
+    private void showDelta(CounterCustom counterCustom1, CounterCustom counterCustom2, CounterCustom counterCustom3,
+                           Map<Integer, Integer> count1, Map<Integer, Integer> count2, Map<Integer, Integer> count3) {
+        System.out.println("*******************************************************");
+        System.out.println("               Delta in %");
+        System.out.println("|          ||| Of method 1 | Of method 2 | Of method 3 | ");
+        System.out.println("|----------|||-------------|-------------|-------------| ");
+        System.out.println(String.format("|  Delta   ||| %11.2f | %11.2f | %11.2f |",
+                (double) counterCustom1.countDelta(count1)*100/TEST_NUMBER,
+                (double) counterCustom2.countDelta(count2)*100/TEST_NUMBER,
+                (double) counterCustom3.countDelta(count3)*100/TEST_NUMBER));
+        System.out.println("|----------|||-------------|-------------|-------------| ");
         System.out.println("");
     }
 
-    private void showStatistics(Map<Integer, Integer> count1, Map<Integer, Integer> count2) {
-        System.out.println("********************************************");
+    private void showStatistics(Map<Integer, Integer> count1, Map<Integer, Integer> count2, Map<Integer, Integer> count3) {
+        System.out.println("*******************************************************");
         System.out.println("               Statistics");
-        System.out.println("|  Element ||| In method 1 | In method 2 | ");
-        System.out.println("|----------|||-------------|-------------| ");
-        for (int i = 1; i <= 7; i++) {
-            System.out.println(String.format("| %8d ||| %11d | %11d |", i, count1.get(i), count2.get(i)));
+        System.out.println("|  Element ||| In method 1 | In method 2 | In method 3 | ");
+        System.out.println("|----------|||-------------|-------------|-------------| ");
+//        for (int i = 1; i <= 7; i++) {
+        for (int i = 0; i <= 8; i++) {
+            System.out.println(String.format("| %8d ||| %11d | %11d | %11d |", i, count1.get(i), count2.get(i), count3.get(i)));
         }
-        System.out.println("|----------------------------------------| ");
+        System.out.println("|----------|||-------------|-------------|-------------| ");
         System.out.println("");
     }
 
@@ -84,7 +111,7 @@ public class Service {
         for (int i = 1; i < TEST_NUMBER; i++) {
             System.out.println(String.format("| %6d | %6d || %8d | %8d |", result1.get(i)[0], result1.get(i)[1], result1.get(i)[2], result2.get(i)[2]));
         }
-        System.out.println("|----------------------------------------| ");
+        System.out.println("|--------|--------||----------|----------| ");
         System.out.println("");
     }
 }
